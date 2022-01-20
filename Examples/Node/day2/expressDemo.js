98adb11377c6c1176c7dd62641a41331e05bd5e8
Kishor <kishor1124@gmail.com>
2022-01-20 16:11:27 +0530

    express js

--- Examples/Node/day2/expressDemo.js
+++ Examples/Node/day2/expressDemo.js
@@ -1,20 +1,28 @@
 let app = require("express")(); //IIFE
+let bodyParser = require('body-parser');
 let port = 9090;
 // handles HTTP GET
-app.get('/', (request, response) => {
-    response.send('Get Request from /');
+// to extract json & text from the body of the request
+app.use([bodyParser.text(), bodyParser.json()]);
+app.get('/employee/:id', (request, response) => {
+    console.log(request.params.id);
+    let employee = {id: 100, name : "Raj", salary : 35000};
+    response.json(employee);
 });
 // handles HTTP POST
-app.post('/', (request, response) => {
-    response.send('Post Request from /')
+app.post('/employee', (request, response) => {
+    console.log(request.body); // reads the request body
+    let content = request.body;
+    response.json(content);
 });
 // handles HTTP PUT
 app.put('/', (request, response) => {
-    response.send('Put Request from /');
+    let employee = {id: 100, name : "Raj", salary : 35000};
+    response.json(employee);
 });
 // handles HTTP DELETE
 app.delete('/', (request, response) => {
-    response.send('Delete Request from /')
+    let employee = {id: 100, name : "Raj", salary : 35000};
+    response.json(employee);
 });
-
 app.listen(port, () => console.log(`listening to ${port}`));
