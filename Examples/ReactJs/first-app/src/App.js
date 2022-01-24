09a2102bea9dada55a90d2bc2c3dcb231805c46e
Kishor <kishor1124@gmail.com>
2022-01-24 14:25:13 +0530

    state & event handling

--- Examples/ReactJs/first-app/src/App.js
+++ Examples/ReactJs/first-app/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
 
+import './App.css';
+import Button, { Input, Simple } from './components/Button';
+import { Counter } from './components/Counter';
 function App() {
+ 
   return (
     <div>
       <h1>Welcome to React with node</h1>
+      <Counter />
     </div>
   );
 }
-
 export default App;
