6b3baaec935092e536fa4aac8f437ba15cc01c5d
Kishor <kishor1124@gmail.com>
2022-01-25 15:25:51 +0530

    angular demos

---
+++ Examples/angular-examples/my-first-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/*
+ selector: tag name
+ templateUrl: content for the selector
+ styleUrls: style for the component
+*/
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-app';
+}
