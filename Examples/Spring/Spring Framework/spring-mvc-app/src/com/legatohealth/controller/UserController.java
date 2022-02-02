281221d183ef344545b5e144b203429008dc07bd
Kishor <kishor1124@gmail.com>
2022-02-02 13:16:21 +0530

    spring mvc app with jdbcTemplate

--- Examples/Spring/Spring Framework/spring-mvc-app/src/com/legatohealth/controller/UserController.java
+++ Examples/Spring/Spring Framework/spring-mvc-app/src/com/legatohealth/controller/UserController.java
@@ -2,10 +2,15 @@ package com.legatohealth.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.legatohealth.beans.User;
+import com.legatohealth.service.UserService;
 /*
  * Controller needs to handle the request and generate the response i.e., view name
  * View must show the dynamic data (Model) i.e., JSP shows some model
@@ -13,6 +18,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	// it needs to read name, password & age from the view & return the response to the view
+	// <input type = 'text' name = 'n1'> <input type = 'text' name = 'n2'>
+	@RequestMapping(value = "/store", method = RequestMethod.POST)
+	public ModelAndView save(@RequestParam("n1") String name, @RequestParam("n2") String password, @RequestParam("n3") int age) {
+		User user = new User();
+		user.setName(name);
+		user.setAge(age);
+		user.setPassword(password);
+		userService.storeUser(user);
+		return new ModelAndView("storeSuccess", "msg", "Stored Successfully");
+	}
 	// application-path/spring/user/datetime
 	@RequestMapping(value = "/datetime", method = RequestMethod.GET)
 	public ModelAndView home() {
