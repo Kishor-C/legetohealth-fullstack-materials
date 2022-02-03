1df3e855637505557db1c8e936c19a63d4bd89bd
Kishor <kishor1124@gmail.com>
2022-02-03 18:13:03 +0530

    spring boot testing

--- Examples/Spring/Spring Framework/spring-boot-app.zip_expanded/spring-boot-app/src/test/java/com/legatohealth/SpringBootAppApplicationTests.java
+++ Examples/Spring/Spring Framework/spring-boot-app.zip_expanded/spring-boot-app/src/test/java/com/legatohealth/SpringBootAppApplicationTests.java
@@ -1,13 +1,30 @@
 package com.legatohealth;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.legatohealth.controller.UserRest;
+import com.legatohealth.service.UserService;
+//this creates an instance of UserRest, and also web environment for testing
+@WebMvcTest(value = UserRest.class) 
 class SpringBootAppApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private UserService service;
+	
 	@Test
-	void contextLoads() {
+	public void testForOkStatus() throws Exception {
+		//service.update() -> result -> controller prepares some result
+		mvc.perform(get("/api/user")).andExpect(status().isOk());
 	}
 
 }
