c3275084a65c0264100f03d7da608987fef16d52
Kishor <kishor1124@gmail.com>
2022-02-07 16:56:55 +0530

    resilience4j

--- Examples/Spring/Spring Microservices/service-client.zip_expanded/service-client/src/main/java/com/example/controller/ServiceClientController.java
+++ Examples/Spring/Spring Microservices/service-client.zip_expanded/service-client/src/main/java/com/example/controller/ServiceClientController.java
@@ -8,27 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.client.Client;
+import com.example.service.MyService;
 
 @RestController
 @RequestMapping("myclient")
 public class ServiceClientController {
 
 	@Autowired
-	private RestTemplate template; // this has methods like get(), post(), put(), delete()
+	private MyService service;
 	
-	@Autowired
-	private Client client; 
+//	@Autowired
+//	private RestTemplate template; // this has methods like get(), post(), put(), delete()
 	
-	@GetMapping("one")
-	public ResponseEntity<Object> callFirstApp() {
-		String url = "http://FIRSTAPP/myfirstapp"; // we are using logical name
-		// getForObject gets the data from the webserivce & 2nd argument is the converted data
-		String data = template.getForObject(url, String.class); 
-		return ResponseEntity.status(200).body("Second App Calling "+data);
-	}
+//	@Autowired
+//	private Client client; 
+	
+//	@GetMapping("one")
+//	public ResponseEntity<Object> callFirstApp() {
+//		String url = "http://FIRSTAPP/myfirstapp"; // we are using logical name
+//		// getForObject gets the data from the webserivce & 2nd argument is the converted data
+//		String data = template.getForObject(url, String.class); 
+//		return ResponseEntity.status(200).body("Second App Calling "+data);
+//	}
 	@GetMapping("oneMore")
 	public ResponseEntity<Object> callFirstAppAgain() {
-		String data = client.invokeFirstApp(); // sends request to http://FIRSTAPP/myfirstapp
-		return ResponseEntity.status(200).body("Second App called through feign "+data);
+		String data = service.getFirstAppData(); // this calls another microservice
+		return ResponseEntity.status(200).body("Result from ServiceClient: "+data);
 	}
 }
